a5d1e65de859fab73ece7184495e738914a6a556
Nikita <123371636+tsomuk@users.noreply.github.com>
2023-02-21 14:25:06 +0200

    code design

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,27 +10,21 @@ import UIKit
 class ViewController: UIViewController {
 
     private var counterValue = 0
-     @IBOutlet private weak var mainTitle: UILabel!
-   
+    @IBOutlet private weak var mainTitle: UILabel!
     @IBOutlet private weak var counterValueLabel: UILabel!
     
     
-     @IBAction private func plustButtonTapped() {
+    @IBAction private func plusButtonTapped() {
         counterValue += 1
         mainTitle.text = "Значение счетчика:"
-         counterValueLabel.text = "\(counterValue)"
-        
+        counterValueLabel.text = "\(counterValue)"
     }
     
-
     @IBAction private func resetButtonTapped() {
         counterValue = 0
         mainTitle.text = "Значение счетчика:"
         counterValueLabel.text = "\(counterValue)"
-        
     }
-    
-    
-    
+
 }
 
